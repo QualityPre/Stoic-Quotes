95b5a36c9f8217e25dd2fb4555b32c007ca3dcfd
QualityPre <qualitypre@protonmail.com>
2022-02-14 13:42:05 +0000

    made responsive

--- scriptjs.js
+++ scriptjs.js
@@ -1,28 +1,28 @@
-'use strict';
-const buttonContainerEl = document.querySelector('.button-container');
-const quoteContainerEl = document.querySelector('.container-words');
-const buttonEl = document.querySelectorAll('.btn--quote');
-const quoteEl = document.querySelector('.quote');
+"use strict";
+const buttonContainerEl = document.querySelector(".button-container");
+const quoteContainerEl = document.querySelector(".container-words");
+const buttonEl = document.querySelectorAll(".btn--quote");
+const quoteEl = document.querySelector(".quote");
 
-const STOIC_API_ONE_URL = 'https://stoicquotesapi.com/v1/api/quotes/random';
-const STOIC_API_TWO_URL = 'https://stoic-server.herokuapp.com/random';
-const STOIC_API_THREE_URL = 'https://stoic-quotes.com/api/quotes?num=1';
+const STOIC_API_ONE_URL = "https://stoicquotesapi.com/v1/api/quotes/random";
+const STOIC_API_TWO_URL = "https://stoic-server.herokuapp.com/random";
+const STOIC_API_THREE_URL = "https://stoic-quotes.com/api/quotes?num=1";
 
 /////////////////ANIMATIONS LOADING/////////////////////
-buttonContainerEl.classList.add('animate__animated', 'animate__fadeInDown');
-buttonContainerEl.style.setProperty('--animate-duration', '1s');
+buttonContainerEl.classList.add("animate__animated", "animate__fadeInDown");
+buttonContainerEl.style.setProperty("--animate-duration", "1s");
 
-quoteContainerEl.classList.add('animate__animated', 'animate__bounceInUp');
-quoteContainerEl.style.setProperty('--animate-duration', '1s');
-quoteContainerEl.addEventListener('animationend', () => {
-  quoteContainerEl.classList.remove('animate__animated', 'animate__bounceInUp');
+quoteContainerEl.classList.add("animate__animated", "animate__bounceInUp");
+quoteContainerEl.style.setProperty("--animate-duration", "1s");
+quoteContainerEl.addEventListener("animationend", () => {
+  quoteContainerEl.classList.remove("animate__animated", "animate__bounceInUp");
 });
 
 ///////////////////////API////////////////////
 function reset(element) {
-  element.textContent = '';
-  element.classList.remove('small');
-  element.classList.remove('very--small');
+  element.textContent = "";
+  element.classList.remove("small");
+  element.classList.remove("very--small");
 }
 
 async function getStoicQuote(url) {
@@ -34,6 +34,7 @@ async function getStoicQuote(url) {
 
     const quote = data.body;
     const author = data.author;
+    if (quote.length > 150) quoteContainerEl.classList.add("small");
 
     createQuote(quote, author);
   } catch (err) {
@@ -50,9 +51,11 @@ async function getStoicQuoteTwo(url) {
 
     let [quoteData] = data;
     const quote = quoteData.body;
+
     const author = quoteData.author;
-    if (quote.length > 250) quoteContainerEl.classList.add('small');
-    if (quote.length > 400) quoteContainerEl.classList.add('very--small');
+    if (quote.length > 150 && quote.length < 300)
+      quoteContainerEl.classList.add("small");
+    if (quote.length > 300) quoteContainerEl.classList.add("very--small");
     createQuote(quote, author);
   } catch (err) {
     console.log(err);
@@ -68,6 +71,7 @@ async function getStoicQuoteThree(url) {
     let [quoteData] = data;
 
     const quote = quoteData.text;
+    if (quote.length > 150) quoteContainerEl.classList.add("small");
     const author = quoteData.author;
 
     createQuote(quote, author);
@@ -76,12 +80,12 @@ async function getStoicQuoteThree(url) {
   }
 }
 
-buttonEl.forEach(btn => {
-  btn.addEventListener('click', e => {
+buttonEl.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
     reset(quoteContainerEl);
-    if (btn.dataset.button === '1') getStoicQuote(STOIC_API_ONE_URL);
-    if (btn.dataset.button === '2') getStoicQuoteTwo(STOIC_API_TWO_URL);
-    if (btn.dataset.button === '3') getStoicQuoteThree(STOIC_API_THREE_URL);
+    if (btn.dataset.button === "1") getStoicQuote(STOIC_API_ONE_URL);
+    if (btn.dataset.button === "2") getStoicQuoteTwo(STOIC_API_TWO_URL);
+    if (btn.dataset.button === "3") getStoicQuoteThree(STOIC_API_THREE_URL);
   });
 });
 
@@ -90,11 +94,11 @@ function createQuote(quote, author) {
     "${quote}"
   </p>
   <p class="author">${author}</p>`;
-  animateCSS('.container-words', 'backInUp');
-  quoteContainerEl.insertAdjacentHTML('afterbegin', markup);
+  animateCSS(".container-words", "backInUp");
+  quoteContainerEl.insertAdjacentHTML("afterbegin", markup);
 }
 
-const animateCSS = (element, animation, prefix = 'animate__') =>
+const animateCSS = (element, animation, prefix = "animate__") =>
   // We create a Promise and return it
   new Promise((resolve, reject) => {
     const animationName = `${prefix}${animation}`;
@@ -106,8 +110,8 @@ const animateCSS = (element, animation, prefix = 'animate__') =>
     function handleAnimationEnd(event) {
       event.stopPropagation();
       node.classList.remove(`${prefix}animated`, animationName);
-      resolve('Animation ended');
+      resolve("Animation ended");
     }
 
-    node.addEventListener('animationend', handleAnimationEnd, { once: true });
+    node.addEventListener("animationend", handleAnimationEnd, { once: true });
   });
